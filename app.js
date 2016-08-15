5caf06e01777f8dce46979e6ce91211e9f22cc53
toktokn <gasua@naver.com>
2016-08-16 05:28:05 +0900

    세번째 커밋

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require("path");
 var app = express();
-app.get("/",function(req,res){
-  res.send("Hello World");
-});
 
-app.listen(3000,function(){
+/*app.get("/",function(req,res){
+  res.send("Hello World");
+});*/
+
+app.use(express.static(path.join(__dirname+"/public")));
+console.log(__dirname);
+
+app.listen(3001,function(){
   console.log("Server On!");
 });
